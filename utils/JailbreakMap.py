89258aa3b27db0d8e60613b879ca8229dd755f5c
mass1ve-err0r <saadatdev@googlemail.com>
2021-01-21 23:58:51 +0100

    Add JailbreakMap --> This is basically an array of dictionaries which hold information about a jailbreaking utility

---
+++ utils/JailbreakMap.py
@@ -0,0 +1,52 @@
+JailbreakMap = [
+    {
+        "name": "checkra1n",
+        "url": "https://checkra.in",
+        "minIOS": "12.0",
+        "maxIOS": "14.3",
+        "minProc": "5S",
+        "maxProc": "8 / X",
+        "type": "Semi-Tethered",
+        "platforms": ['apple', 'linux']
+    },
+    {
+        "name": "Odyssey",
+        "url": "https://theodyssey.dev",
+        "minIOS": "13.0",
+        "maxIOS": "13.7",
+        "minProc": "6S",
+        "maxProc": "11",
+        "type": "Semi-Untethered",
+        "platforms": ['apple']
+    },
+    {
+        "name": "unc0ver",
+        "url": "https://unc0ver.dev",
+        "minIOS": "11.0",
+        "maxIOS": "13.5",
+        "minProc": "5S",
+        "maxProc": "11",
+        "type": "Semi-Untethered",
+        "platforms": ['apple']
+    },
+    {
+        "name": "Chimera",
+        "url": "https://chimera.coolstar.org",
+        "minIOS": "12.0",
+        "maxIOS": "12.5.1",
+        "minProc": "5S",
+        "maxProc": "XS",
+        "type": "Semi-Untethered",
+        "platforms": ['apple']
+    },
+    {
+        "name": "Electra",
+        "url": "https://coolstar.org/electra/",
+        "minIOS": "11.0",
+        "maxIOS": "11.4.1",
+        "minProc": "5S",
+        "maxProc": "X",
+        "type": "Semi-Untethered",
+        "platforms": ['apple']
+    }
+]
