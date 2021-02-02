aaa36fa94b81f7d3c9b5bb397cbab7bafd5a930a
mass1ve-err0r <me@saadat.dev>
2021-02-02 16:50:23 +0100

    Unify JBMap & DMap as SiteHelper

--- utils/SiteHelper.py
+++ utils/SiteHelper.py
@@ -1,3 +1,7 @@
+# Device Mappings based on Processor
+DeviceMap = ["2G", "3G", "3GS", "4", "4S", "5", "5S", "6", "6S", "7", "8", "X", "XS", "11", "12"]
+
+# Jailbreak Tools
 JailbreakMap = [
     {
         "name": "checkra1n",
