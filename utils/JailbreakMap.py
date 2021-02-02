9a492d8484bcfb905f0c3b49f290e7bfdc1626d5
mass1ve-err0r <me@saadat.dev>
2021-02-02 01:17:22 +0100

    Update JailbreakMap + added safe jailbreaks from iOS 10 to iOS 6 --> Links might need to be reworked!

--- utils/JailbreakMap.py
+++ utils/JailbreakMap.py
@@ -48,5 +48,175 @@ JailbreakMap = [
         "maxProc": "X",
         "type": "Semi-Untethered",
         "platforms": ['apple']
+    },
+    {
+        "name": "TotallyNotSpyware",
+        "url": "https://totally-not.spyware.lol",
+        "minIOS": "10.0",
+        "maxIOS": "10.3.3",
+        "minProc": "5S",
+        "maxProc": "7",
+        "type": "Semi-Untethered",
+        "platforms": ['apple']
+    },
+    {
+        "name": "h3lix",
+        "url": "https://h3lix.tihmstar.net/",
+        "minIOS": "10.0",
+        "maxIOS": "10.3.4",
+        "minProc": "5",
+        "maxProc": "5",
+        "type": "Semi-Untethered",
+        "platforms": ['apple']
+    },
+    {
+        "name": "doubleh3lix",
+        "url": "https://h3lix.tihmstar.net/",
+        "minIOS": "10.0",
+        "maxIOS": "10.3.4",
+        "minProc": "5S",
+        "maxProc": "6S",
+        "type": "Semi-Untethered",
+        "platforms": ['apple']
+    },
+    {
+        "name": "Meridian",
+        "url": "https://meridian.sparkes.zone/",
+        "minIOS": "10.0",
+        "maxIOS": "10.3.3",
+        "minProc": "5S",
+        "maxProc": "7",
+        "type": "Semi-Untethered",
+        "platforms": ['apple']
+    },
+    {
+        "name": "Pangu9",
+        "url": "http://en.pangu.io/",
+        "minIOS": "9.0",
+        "maxIOS": "9.3.3",
+        "minProc": "5S",
+        "maxProc": "6S",
+        "type": "Semi-Untethered",
+        "platforms": ['windows', 'apple']
+    },
+    {
+        "name": "Home Depot",
+        "url": "http://wall.supplies/OLD%20iPhone%20HACKED.html",
+        "minIOS": "9.1",
+        "maxIOS": "9.3.4",
+        "minProc": "4S",
+        "maxProc": "5",
+        "type": "Semi-Untethered",
+        "platforms": ['apple']
+    },
+    {
+        "name": "JailbreakMe 4.0",
+        "url": "http://jailbreak.me",
+        "minIOS": "9.1",
+        "maxIOS": "9.3.4",
+        "minProc": "4S",
+        "maxProc": "5",
+        "type": "Semi-Untethered",
+        "platforms": ['apple']
+    },
+    {
+        "name": "Phoenix",
+        "url": "https://www.phoenixpwn.com",
+        "minIOS": "9.3.5",
+        "maxIOS": "9.3.6",
+        "minProc": "4S",
+        "maxProc": "5",
+        "type": "Semi-Untethered",
+        "platforms": ['apple']
+    },
+    {
+        "name": "TaiG",
+        "url": "http://www.taig.com/en/",
+        "minIOS": "8.0",
+        "maxIOS": "8.4",
+        "minProc": "4S",
+        "maxProc": "6",
+        "type": "Untethered",
+        "platforms": ['windows', 'apple']
+    },
+    {
+        "name": "Pangu8",
+        "url": "http://en.8.pangu.io/",
+        "minIOS": "8.0",
+        "maxIOS": "8.1",
+        "minProc": "4S",
+        "maxProc": "6",
+        "type": "Untethered",
+        "platforms": ['windows', 'apple']
+    },
+    {
+        "name": "evasi0n7",
+        "url": "http://evasi0n.com",
+        "minIOS": "7.0",
+        "maxIOS": "7.0.6",
+        "minProc": "4",
+        "maxProc": "5S",
+        "type": "Untethered",
+        "platforms": ['windows', 'apple']
+    },
+    {
+        "name": "Pangu",
+        "url": "https://en.7.pangu.io",
+        "minIOS": "7.1",
+        "maxIOS": "7.1.2",
+        "minProc": "4",
+        "maxProc": "5S",
+        "type": "Untethered",
+        "platforms": ['windows', 'apple']
+    },
+    {
+        "name": "evasi0n",
+        "url": "http://evasi0n.com/iOS6",
+        "minIOS": "6.0",
+        "maxIOS": "6.1.2",
+        "minProc": "3GS",
+        "maxProc": "5",
+        "type": "Untethered",
+        "platforms": ['windows', 'apple', 'linux']
+    },
+    {
+        "name": "sn0wbreeze",
+        "url": "https://ih8sn0w.com",
+        "minIOS": "6.0",
+        "maxIOS": "6.1.3",
+        "minProc": "3GS",
+        "maxProc": "4",
+        "type": "Mixed",
+        "platforms": ['windows']
+    },
+    {
+        "name": "p0sixpwn",
+        "url": "https://www.theiphonewiki.com/wiki/P0sixspwn",
+        "minIOS": "6.1.3",
+        "maxIOS": "6.1.6",
+        "minProc": "3GS",
+        "maxProc": "5",
+        "type": "Mixed",
+        "platforms": ['windows', 'apple']
+    },
+    {
+        "name": "redsn0w",
+        "url": "https://www.theiphonewiki.com/wiki/P0sixspwn",
+        "minIOS": "6.0",
+        "maxIOS": "6.1.6",
+        "minProc": "3GS",
+        "maxProc": "4",
+        "type": "Mixed",
+        "platforms": ['windows', 'apple']
+    },
+    {
+        "name": "ipwndfu",
+        "url": "https://github.com/axi0mX/ipwndfu/",
+        "minIOS": "6.0",
+        "maxIOS": "6.1.6",
+        "minProc": "3GS",
+        "maxProc": "3GS",
+        "type": "Mixed",
+        "platforms": ['apple', 'linux']
     }
 ]
