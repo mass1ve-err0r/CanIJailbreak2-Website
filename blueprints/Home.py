4ae878e4caea464364550d4f8ed36bb9818a75d0
mass1ve-err0r <me@saadat.dev>
2021-02-03 19:46:07 +0100

    Update Home blueprint + Guide Template + added same boundary checks from the API + added dirty iPadPro name parser --> this is a needed evil for now, this is subject to change!

--- blueprints/Home.py
+++ blueprints/Home.py
@@ -5,7 +5,9 @@ from sanic.response import html, json
 from sanic.exceptions import NotFound
 
 from utils.SiteHelper import JailbreakMap as jMap
-from utils.SiteHelper import DeviceMap as dMap
+from utils.SiteHelper import DeviceMapPG as dMap
+from utils.SiteHelper import MinVersionMap as minMap
+from utils.SiteHelper import MaxVersionMap as maxMap
 
 
 HomeBP = Blueprint("HomeBP")
@@ -18,6 +20,24 @@ _tools9 = [x for x in jMap if parse(x.get('minIOS')) <= parse("9.0") <= parse(x.
 _tools8 = [x for x in jMap if parse(x.get('minIOS')) <= parse("8.0") <= parse(x.get('maxIOS')) or parse("8.0") <= parse(x.get('maxIOS')) <= parse("8.4")]
 _tools7 = [x for x in jMap if parse(x.get('minIOS')) <= parse("7.0") <= parse(x.get('maxIOS')) or parse("7.0") <= parse(x.get('maxIOS')) <= parse("7.1.2")]
 _tools6 = [x for x in jMap if parse(x.get('minIOS')) <= parse("6.0") <= parse(x.get('maxIOS')) or parse("6.0") <= parse(x.get('maxIOS')) <= parse("6.1.6")]
+
+
+# -*- Helper func -*-
+async def format_ipad_name(name):
+    if name == "iPad Pro 12.9 1":
+        return "iPad Pro (12.9) (1.Generation)"
+    elif name == "iPad Pro 12.9 2":
+        return "iPad Pro (12.9) (2.Generation)"
+    elif name == "iPad Pro 12.9 3":
+        return "iPad Pro (12.9) (3.Generation)"
+    elif name == "iPad Pro 12.9 4":
+        return "iPad Pro (12.9) (4.Generation)"
+    elif name == "iPad Pro 11 1":
+        return "iPad Pro (11) (1.Generation)"
+    elif name == "iPad Pro 11 2":
+        return "iPad Pro (11) (2.Generation)"
+    else:
+        return name
 
 
 # -*- Routes -*-
@@ -48,19 +68,28 @@ async def guide_me(request):
             _html = await template.render_async(title="Jailbreak Wizard | Can I Jailbreak2",
                                                 showFieldError=True)
         else:
+            _found = False
             ident_device_split = ident_device_full.split('-')
             ident_device = ident_device_split[0]
-            ident_device_name = ident_device_split[1]
-            _found = False
-            map_supported = [x for x in jMap if (parse(x.get('minIOS')) <= parse(ident_ios) <= parse(x.get('maxIOS'))) \
-                             and (dMap.index(x.get('minProc')) <= dMap.index(ident_device) <= dMap.index(x.get('maxProc')))]
-            if len(map_supported) != 0:
-                _found = True
-            _html = await template.render_async(title="Jailbreak Wizard | Can I Jailbreak2",
-                                                showResult=_found,
-                                                iosV=ident_ios,
-                                                dev=ident_device_name,
-                                                jelbreks=map_supported)
+            ident_device_name = await format_ipad_name(ident_device_split[1])
+            if parse(minMap.get(ident_device_name)) <= parse(ident_ios) <= parse(maxMap.get(ident_device_name)):
+                map_supported = [x for x in jMap if
+                                 (parse(x.get('minIOS')) <= parse(ident_ios) <= parse(x.get('maxIOS'))) \
+                                 and (dMap.index(x.get('minProc')) <= dMap.index(ident_device) <= dMap.index(
+                                     x.get('maxProc')))]
+                if len(map_supported) != 0:
+                    _found = True
+                _html = await template.render_async(title="Jailbreak Wizard | Can I Jailbreak2",
+                                                    showResult=_found,
+                                                    iosV=ident_ios,
+                                                    dev=ident_device_name,
+                                                    jelbreks=map_supported)
+            else:
+                _html = await template.render_async(title="Jailbreak Wizard | Can I Jailbreak2",
+                                                    showResult=_found,
+                                                    iosV=ident_ios,
+                                                    dev=ident_device_name,
+                                                    jelbreks=[])
     elif request.method == 'GET':
         _html = await template.render_async(title="Jailbreak Wizard | Can I Jailbreak2")
     return html(_html)
