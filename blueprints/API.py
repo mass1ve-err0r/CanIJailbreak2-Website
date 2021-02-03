a0fb8b4adbf7f22fe249c920848711b6fdf7e7f8
mass1ve-err0r <me@saadat.dev>
2021-02-03 01:58:06 +0100

    Update API blueprint + add min version check

--- blueprints/API.py
+++ blueprints/API.py
@@ -7,6 +7,7 @@ from sanic.response import json
 from utils.SiteHelper import JailbreakMap as jMap
 from utils.SiteHelper import DeviceMap as dMap
 from utils.SiteHelper import APIMap as aMap
+from utils.SiteHelper import APIMinVersionMap as mMap
 
 APIBP = Blueprint("APIBP", version="v1")
 valid_tokens = [t for t in env.get("VALID_TOKENS").split("\n")]
@@ -29,6 +30,8 @@ async def home2(request, device, iosv):
             dev = await get_parsed_device(device)
             if dev == None:
                 return json({"status": 1})
+            if parse(iosv) < parse(mMap.get(device)):
+                return json({"status": 2})
             map_supported = [x for x in jMap if (parse(x.get('minIOS')) <= parse(iosv) <= parse(x.get('maxIOS'))) \
                              and (dMap.index(x.get('minProc')) <= dMap.index(dev) <= dMap.index(x.get('maxProc')))]
             return json(
