b1a9d9ea63d823d922e1759778e900900621be0f
mass1ve-err0r <me@saadat.dev>
2021-04-24 16:44:55 +0200

    add cleaned server

--- server.py
+++ server.py
@@ -1,42 +1,49 @@
 from dotenv import load_dotenv
-load_dotenv()
-
 from sanic import Sanic
+from sanic.response import empty
+from sanic.exceptions import SanicException
 from jinja2 import Environment, PackageLoader, select_autoescape
-from blueprints.Home import HomeBP
-from blueprints.API import APIBP
+
+# Environment prep before any stuff loads.
+load_dotenv("./.env")
+
+from blueprints.general import GeneralBP
 
 
-# -*- Sanic configg 4 fest deploiment -*-
+# -*- Server Config -*-
 app = Sanic(__name__)
 app.static('/static', './static')
 app.config.update({
     # "PROXIES_COUNT": 1,
-    # "REAL_IP_HEADER": "X-Real-IP",
-    # "FORWARDED_SECRET": "placeholder_secret"
+    # "FORWARDED_SECRET": "your_secret"
 })
 
 
-# -*- Jinja2 setup -*-
-J2env = Environment(loader=PackageLoader('server', './templates'),
+# -*- Jinja2 Setup -*-
+J2Env = Environment(loader=PackageLoader('server', './templates'),
                     autoescape=select_autoescape(['html', 'xml']),
                     enable_async=True)
-J2env.globals["url_for"] = app.url_for
-app.J2env = J2env
+J2Env.globals["url_for"] = app.url_for
+app.ctx.J2Env = J2Env
+
+
+# -*- DEBUG ALL Exception Handler -*-
+@app.exception(SanicException)
+async def catch_exceptions_4xx_5xx(req, excp):
+    return empty(status=999)
 
 
 # -*- Blueprint Registration -*-
-app.blueprint(HomeBP)
-app.blueprint(APIBP)
+app.blueprint(GeneralBP)
 
 
 # -*- DEBUG HEADERS -*-
 @app.middleware('response')
-async def debug_headers(request, response):
-    response.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
-    response.headers['Pragma'] = "no-cache"
-    response.headers['Expires'] = "0"
+async def debug_headers(req, res):
+    res.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
+    res.headers['Pragma'] = "no-cache"
+    res.headers['Expires'] = "0"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, access_log=True, debug=True)
+    app.run(host="0.0.0.0", port=8000, workers=2, access_log=True, debug=True)
