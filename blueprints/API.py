03d8930bb1328d5e05d21cebceb45fa124b8c3ab
mass1ve-err0r <me@saadat.dev>
2021-04-25 13:42:19 +0200

    add api blueprint * added changes to accommodate for sanic 21+ + added null check if no device found * cleanup code and improve readability

--- blueprints/API.py
+++ blueprints/API.py
@@ -1,52 +1,54 @@
 # -*- Blueprint setup -*-
 from os import environ as env
+from urllib.parse import unquote_plus
+from sanic import Blueprint, response
 from packaging.version import parse
-from sanic import Blueprint
-from sanic.response import json
+from utils.jailbreaks import JailbreakMap as jMap
+from utils.device_helper import DeviceMapPG as dMap
+from utils.device_helper import DeviceMapPGNamed as aMap
+from utils.device_helper import MinVersionMap as minMap
+from utils.device_helper import MaxVersionMap as maxMap
 
-from utils.SiteHelper import JailbreakMap as jMap
-from utils.SiteHelper import DeviceMapPG as dMap
-from utils.SiteHelper import DeviceMapPGNamed as aMap
-from utils.SiteHelper import MinVersionMap as minMap
-from utils.SiteHelper import MaxVersionMap as maxMap
-
-APIBP = Blueprint("APIBP", version="v1")
-valid_tokens = [t for t in env.get("VALID_TOKENS").split("\n")]
+APIBP = Blueprint('APIBP', version=1)
+validTokens = [t for t in env.get("TOKENS").split("\n")]
 
 
 # -*- Helper -*-
 async def get_parsed_device(device):
     rv = [k for k, v in aMap.items() if device in v]
+    if len(rv) == 0:
+        return None
     return rv[-1]
 
 
-# -*- Routes -*-
-@APIBP.route('/pls/<device>/<iosv>')
-async def home2(request, device, iosv):
-    _token = request.headers.get('Authorization')
-    if _token == None:
-        return json({"status": -1})
+# -*- Standard Routes -*-
+@APIBP.route('/pls/<device_name>/<iosv>', methods=['GET'])
+async def home2(request, device_name, iosv):
+    device = unquote_plus(device_name)
+    potentialToken = request.headers.get('Authorization')
+    if potentialToken is None:
+        return response.json({"status": -1})
     else:
-        if _token in valid_tokens:
-            dev = await get_parsed_device(device)
-            if dev == None:
-                return json({"status": 1})
+        if potentialToken in validTokens:
+            potentialDevice = await get_parsed_device(unquote_plus(device))
+            if potentialDevice == None:
+                return response.json({"status": 1})
             if parse(minMap.get(device)) <= parse(iosv) <= parse(maxMap.get(device)):
-                map_supported = [x for x in jMap if (parse(x.get('minIOS')) <= parse(iosv) <= parse(x.get('maxIOS'))) \
-                                 and (dMap.index(x.get('minProc')) <= dMap.index(dev) <= dMap.index(x.get('maxProc')))]
-                return json(
+                supportedTools = [x for x in jMap if (parse(x.get('minimum_ios')) <= parse(iosv) <= parse(x.get('maximum_ios'))) \
+                                  and (dMap.index(x.get('minimum_pg')) <= dMap.index(potentialDevice) <= dMap.index(x.get('maximum_pg')))]
+                return response.json(
                     {
                         "status": 0,
-                        "jelbreks": map_supported
+                        "jelbreks": supportedTools
                     }
                 )
             else:
-                return json(
+                return response.json(
                     {
                         "status": 2,
-                        "minIOS": minMap.get(device),
-                        "maxiOS": maxMap.get(device)
+                        "minimum_ios": minMap.get(device),
+                        "maximum_ios": maxMap.get(device)
                     }
                 )
         else:
-            return json({"status": -1})
+            return response.json({"status": -1})
